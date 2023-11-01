36b547d2063ebb9670fa86510cd4bdd5a9ca0b36
shargrave38 <144182890+shargrave38@users.noreply.github.com>
2023-11-01 12:19:13 -0400

    added create

---
+++ create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+	user_id INT(11) NOT NULL AUTO_INCREMENT,
+	user_name VARCHAR(20) NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	sign_up_date DATETIME NOT NULL
+);
