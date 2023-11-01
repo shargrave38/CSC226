ec7dc955e27cc476b7d6f99d7987e94211fcf43c
shargrave38 <144182890+shargrave38@users.noreply.github.com>
2023-11-01 12:26:22 -0400

    new table added

--- create.sql
+++ create.sql
@@ -1,7 +1,3 @@
-CREATE TABLE users (
-	user_id INT(11) NOT NULL AUTO_INCREMENT,
-	user_name VARCHAR(20) NOT NULL,
-	password VARCHAR(20) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	sign_up_date DATETIME NOT NULL
-);
+INSERT INTO users (user_name, password, email, sign_up_date) VALUES ("sahirag3", "password", "sahirag3@gmail.com", "2003-02-23 21:54:03");
+INSERT INTO users (user_name, password, email, sign_up_date) VALUES ("johndoe", "johndoe123", "johndoegmail.com", "1992-11-17 07:30:57");
+INSERT INTO users (user_name, password, email, sign_up_date) VALUES ("samAltman", "sammyAltman", "samAltmangmail.com", "1930-04-26 00:22:08");
