c67fd1d1bd8127e9f52c07e737157d7cac7d86db
shargrave38 <144182890+shargrave38@users.noreply.github.com>
2023-11-06 11:31:21 -0500

    read fix

--- read.sql
+++ read.sql
@@ -1,3 +1,3 @@
 SELECT * FROM users;
 SELECT * FROM users WHERE password = 'password';
-SELECT user_name, email FROM users WHERE sing_up_date ='1930-04-26 00:22:08;
+SELECT user_name, email FROM users WHERE sing_up_date ='1930-04-26 00:22:08';
